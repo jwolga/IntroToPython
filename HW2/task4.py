03debf336b402150d9e66f4657102ea17d7f4c07
Olga Kovalenko <jwolga@yandex.ru>
2022-12-01 19:14:46 +0200

    Added task 4

--- HW2/task4.py
+++ HW2/task4.py
@@ -3,3 +3,15 @@
 -2 -1 0 1 2
 Позиции: 0,1 -> 2
 '''
+import random
+
+N = int(input("Введите число N: "))
+l = []
+for i in range(-N, N+1):
+    l.append(i)
+lowBorder = random.randint(0, N-1)
+hiBorder = random.randint(N, 2*N-1)
+for p in l:
+    print(p)
+print(f"Произведение l[{lowBorder}] элемента ({l[lowBorder]}) на l[{hiBorder}] элемента ({l[hiBorder]}) равно {l[lowBorder] * l[hiBorder]}")
+
