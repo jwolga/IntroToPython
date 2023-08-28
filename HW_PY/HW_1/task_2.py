912afbc6382e372ee923bdd749dccc5a3fa37295
Olga Kovalenko <jwolga@yandex.ru>
2023-08-28 10:55:05 +0300

    old

--- HW_PY/HW_1/task_2.py
+++ HW_PY/HW_1/task_2.py
@@ -3,11 +3,22 @@
 Пример:
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0)
-'''
+
 number = int(input('Введите трехзначное число: '))
 first_dig = number // 100
 value = number % 100
 second_dig = value // 10
 third_dig = value % 10
 print(f'Сумма цифр числа {number}: {first_dig + second_dig + third_dig}')
+'''
+a = {0: "Л", 1: "Н", 2: "Р", 3: "Т"}
+k = 0
+for i in range(0, len(a)):
+    for j in range(0, len(a)):
+        for g in range(0, len(a)):
+            for m in range(0, len(a)):
+                for n in range(0, len(a)):
+                    k += 1 # k = k + 1
+                    if k == 150:
+                        print(a[i], a[j], a[g], a[m], a[n], end=" ")
 
